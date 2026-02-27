f6e73a6b11cdc9792d0d08c44f1a1fe5be348763
Orestis Sabethai <o.r.e.s.t.i.s@hotmail.com>
2026-02-28 00:26:39 +0200

    docs: add 6-month coding skill independence study plan

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -1,45 +1,44 @@
 class Node:
-    __slots__ = ("value", "prev", "next")
+    __slots__ = ("value", "next", "previous")
 
     def __init__(self, value=None):
         self.value = value
-        self.prev = None
         self.next = None
+        self.previous = None
 
 
 class DoublyLinkedList:
     def __init__(self):
-        # Sentinel nodes (Always present, never removed)
         self.dummy_head = Node()
         self.dummy_tail = Node()
         self.dummy_head.next = self.dummy_tail
-        self.dummy_tail.prev = self.dummy_head
+        self.dummy_tail.previous = self.dummy_head
 
     def insert_front(self, value):
         new = Node(value)
         old_first = self.dummy_head.next
-        new.prev = self.dummy_head
         new.next = old_first
+        new.previous = self.dummy_head
         self.dummy_head.next = new
-        old_first.prev = new
+        old_first.previous = new
         return new
 
     def insert_back(self, value):
         new = Node(value)
-        old_last = self.dummy_tail.prev
-        new.prev = old_last
+        old_last = self.dummy_tail.previous
         new.next = self.dummy_tail
+        new.previous = old_last
         old_last.next = new
-        self.dummy_tail.prev = new
+        self.dummy_tail.previous = new
         return new
 
     def delete(self, node):
-        prev_node = node.prev
+        previous_node = node.previous
         next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-        node.prev = None
+        previous_node.next = next_node
+        next_node.previous = previous_node
         node.next = None
+        node.previous = None
         return node.value
 
     def traverse_forward(self):
@@ -52,17 +51,17 @@ class DoublyLinkedList:
 
     def traverse_backward(self):
         values = []
-        current = self.dummy_tail.prev
+        current = self.dummy_tail.previous
         while current is not self.dummy_head:
             values.append(current.value)
-            current = current.prev
+            current = current.previous
         return values
 
     def reverse(self):
         current = self.dummy_head
         while current is not None:
-            current.prev, current.next = current.next, current.prev
-            current = current.prev
+            current.next, current.previous = current.previous, current.next
+            current = current.previous
         self.dummy_head, self.dummy_tail = self.dummy_tail, self.dummy_head
 
     def __repr__(self):
@@ -82,9 +81,11 @@ def main():
     print(dll.traverse_backward())  # expect [2, 1, 3]
 
     node = dll.insert_front(99)
-    print(dll.traverse_forward())  # expect [99, 3, 1, 2]
+    node1 = dll.insert_back(100)
+    print(dll.traverse_forward())  # expect [99, 3, 1, 2, 100]
     dll.delete(node)
-    print(dll.traverse_forward())  # expect [3, 1, 2]
+    dll.delete(node1)
+    print(dll)
 
 
 if __name__ == "__main__":
